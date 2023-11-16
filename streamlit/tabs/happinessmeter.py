e3e0c581e1d3da0c5dab34c718a6b9506046794a
Étienne Breton <skylerauraarena@gmail.com>
2023-11-16 13:28:54 +0100

    Ajout des tableaux de pays proches pour chaque variable

--- streamlit/tabs/happinessmeter.py
+++ streamlit/tabs/happinessmeter.py
@@ -6,19 +6,22 @@ from joblib import load
 import os
 from sklearn.preprocessing import MinMaxScaler
 import pickle
+import warnings
+warnings.filterwarnings("ignore", category=FutureWarning)
 
 title = "Happiness Meter"
 sidebar_name = "Happiness Meter"
 prediction = "Happiness Score :"
 
+cols=['Log GDP per capita','Social support','Healthy life expectancy at birth','Freedom to make life choices','Generosity',
+    'Perceptions of corruption','Positive affect','Negative affect']
+
 dir_path = os.path.dirname(os.path.realpath(__file__))
 csv_path = os.path.join(dir_path, "../../data/data2022.csv")
 df_country = pd.read_csv(csv_path, sep=';')
 
-def normalize(df):
+def normalize_array(df):
     scaler=MinMaxScaler()
-    cols=['Log GDP per capita','Social support','Healthy life expectancy at birth','Freedom to make life choices','Generosity',
-        'Perceptions of corruption','Positive affect','Negative affect']
     dir_path = os.path.dirname(os.path.realpath(__file__))
     scaler_path = os.path.join(dir_path, "../../models/scaler.pkl")
     loaded_scaler = load(scaler_path)
@@ -50,20 +53,18 @@ def predict(continent, gdp, socsup, life_exp, freedom, generosity, corruption, p
     })
     if continent != 'Central and eastern Europe':
         X_test['Regional indicator_' + continent] = 1
-    X_test = normalize(X_test)
+    X_test = normalize_array(X_test)
     # Chemin absolu vers le dossier contenant app.py
     model_path = os.path.join(dir_path, "../../models/happinessmeter.joblib")
     loaded_model = load(model_path)
     return loaded_model.predict(X_test)
 
-def find_closest_countries(predicted_value, df, num_countries=5):
+def find_life_ladder_closest_countries(predicted_value, df, num_countries=5):
     # Calculer la différence absolue entre la valeur prédite et chaque valeur dans le dataframe
     # Nettoyer la chaîne de caractères pour enlever les crochets
     predicted_value = predicted_value.strip("[]")
-
     # Convertir la chaîne de caractères nettoyée en flottant
     predicted_value = float(predicted_value)
-
     # Convertir la colonne en string si ce n'est pas déjà le cas
     if df['Life Ladder'].dtype != 'O':  # 'O' signifie object
         df['Life Ladder'] = df['Life Ladder'].astype(str)
@@ -80,6 +81,33 @@ def find_closest_countries(predicted_value, df, num_countries=5):
     closest_countries = df.sort_values('Difference').head(num_countries)
     return closest_countries
 
+def find_closest_countries(predicted_values, df, num_countries=5):
+    # Calcule la différence absolue entre les variables et chaque valeur dans le dataframe
+    closest_countries = []
+    # Normalisation des valeurs
+    values_dict = {col: [val] for col, val in zip(cols, predicted_values)}
+    df_predicted_values = pd.DataFrame(values_dict)
+    df_predicted_values = normalize_array(df_predicted_values)
+    # Transforme le dataframe en liste
+    predicted_values = df_predicted_values.values.tolist()[0]
+    for i, v in enumerate(cols):
+        # Convertion de la chaîne de caractères nettoyée en flottant
+        predicted_value = float(predicted_values[i])
+        # Convertion de la colonne en string si ce n'est pas déjà le cas
+        if df[v].dtype != 'O':  # 'O' signifie object
+            df[v] = df[v].astype(str)
+        # Remplacement des virgules par des points
+        df[v] = df[v].str.replace(',', '.')
+        df = df.sort_values(by=v, ascending=False).reset_index(drop=True)
+        df.index = df.index + 1
+        # Convertion de la colonne en valeurs numériques
+        df[v] = pd.to_numeric(df[v], errors='coerce')
+        # Calculer la différence
+        df['Difference'] = df[v].apply(lambda x: abs(x - predicted_value))
+        # Trie le dataframe par différence et prendre les premiers pays
+        closest_countries.append(df.sort_values('Difference').head(num_countries))
+    return closest_countries
+
 def run():
     st.title(title)
 
@@ -87,22 +115,26 @@ def run():
     '''
     st.text(multi)
 
-    continent = st.selectbox(
-        'Select the continental area of the country you want to predict the happiness score:',
-    ('Central and eastern Europe', 'Commonwealth of Independent States', 'East Asia', 'Latin America and Caribbean', 'Middle East and North Africa', 'North America and ANZ', 'South Asia', 'Southeast Asia', 'Sub-Saharan Africa', 'Western Europe'))
-    gdp = st.slider('Log GDP per capita', 0.00, 13.00, 9.38)
-    socsup = st.slider('Social support level', 0.00, 10.00, 8.13)
-    life_exp = st.slider('Healthy life expectancy at birth', 0.00, 100.00, 63.60)
-    freedom = st.slider('Freedom to make life choices', 0.00, 10.00, 7.47)
-    generosity = st.slider('Generosity', -1.00, 10.00, -0.01)
-    corruption = st.slider('Perception of corruption', 0.00, 10.00, 7.40)
-    positive = st.slider('Positive affects', 0.00, 10.00, 7.10)
-    negative = st.slider('Negative affects', 0.00, 10.00, 2.68)
-    trigger = st.button("Prédire", type="primary")
+    continent = st.selectbox(label='Select the continental area of the country you want to predict the happiness score:',
+    options=('Central and eastern Europe', 'Commonwealth of Independent States', 'East Asia', 'Latin America and Caribbean', 'Middle East and North Africa', 'North America and ANZ', 'South Asia', 'Southeast Asia', 'Sub-Saharan Africa', 'Western Europe'))
+    gdp = st.slider(label='Log GDP per capita', min_value=0.00, max_value=13.00, value=9.38)
+    socsup = st.slider(label='Social support level', min_value=0.00, max_value=10.00, value=8.13) / 10
+    life_exp = st.slider(label='Healthy life expectancy at birth', min_value=0.00, max_value=100.00, value=63.60)
+    freedom = st.slider(label='Freedom to make life choices', min_value=0.00, max_value=10.00, value=7.47) / 10
+    generosity = st.slider(label='Generosity', min_value=-1.00, max_value=10.00, value=-0.01) / 10
+    corruption = st.slider(label='Perception of corruption', min_value=0.00, max_value=10.00, value=7.40) / 10
+    positive = st.slider(label='Positive affects', min_value=0.00, max_value=10.00, value=7.10) / 10
+    negative = st.slider(label='Negative affects', min_value=0.00, max_value=10.00, value=2.68) / 10
+    trigger = st.button(label="Prédire", type="primary")
     if trigger:
         prediction = str(predict(continent, gdp, socsup, life_exp, freedom, generosity, corruption, positive, negative))
         st.write('Le niveau de bien-être prédit est de ', prediction.strip("[]"))
-        # Comparer avec les pays du dataframe
-        closest_countries = find_closest_countries(prediction, df_country)
+        # Compare avec les pays du dataframe
+        life_ladder_closest_countries = find_life_ladder_closest_countries(prediction, df_country)
         st.write(f'Pays ayant eu un niveau de bien-être le plus proche en 2022 (sur {len(df_country)} pays) :')
-        st.dataframe(closest_countries[['Country name', 'Life Ladder','Difference']])
+        st.dataframe(life_ladder_closest_countries[['Country name', 'Life Ladder','Difference']])
+
+        closest_countries = find_closest_countries([gdp, socsup, life_exp, freedom, generosity, corruption, positive, negative], df_country)
+        for i in range(len(cols) - 1):
+            st.write(f'Pays ayant eu un niveau de {cols[i]} le plus proche en 2022 (sur {len(df_country)} pays) :')
+            st.dataframe(closest_countries[i][['Country name', 'Life Ladder', cols[i], 'Difference']])
